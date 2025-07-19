42e4aaa2f0db738bf71a8a30f2f4071d92a246de
makisacik <glsabaton@outlook.com>
2025-07-20 00:56:55 +0300

    Fetch products

--- MEHMETALİ_KISACIK.js
+++ MEHMETALİ_KISACIK.js
@@ -9,6 +9,26 @@
       console.log("wrong page");
       return;
     }
+
+    let products = await fetchProducts();
+    console.log("Fetched products:", products);
+  };
+
+  const fetchProducts = async () => {
+    try {
+      const res = await fetch(
+        "https://gist.githubusercontent.com/sevindi/8bcbde9f02c1d4abe112809c974e1f49/raw/9bf93b58df623a9b16f1db721cd0a7a539296cf0/products.json"
+      );
+      const data = await res.json();
+      if (!Array.isArray(data)) {
+        console.error("Fetched data is not an array", data);
+        return [];
+      }
+      return data;
+    } catch (err) {
+      console.error("Failed to fetch product data", err);
+      return [];
+    }
   };
 
   init();
