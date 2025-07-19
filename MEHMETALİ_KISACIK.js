978fabad350faa45c81f7c16455b124feed5c215
makisacik <glsabaton@outlook.com>
2025-07-20 00:58:24 +0300

    Cache the product data

--- MEHMETALİ_KISACIK.js
+++ MEHMETALİ_KISACIK.js
@@ -1,11 +1,9 @@
 /** @format */
 (() => {
+  const LOCAL_KEY = "cachedProducts";
+
   const init = async () => {
-    if (
-      location.pathname !== "/" &&
-      location.pathname !== "home" &&
-      location.pathname !== "/index.html"
-    ) {
+    if (location.pathname !== "/" && location.pathname !== "/index.html") {
       console.log("wrong page");
       return;
     }
@@ -15,6 +13,17 @@
   };
 
   const fetchProducts = async () => {
+    const cached = localStorage.getItem(LOCAL_KEY);
+    if (cached) {
+      try {
+        const parsed = JSON.parse(cached);
+        if (Array.isArray(parsed)) return parsed;
+        else localStorage.removeItem(LOCAL_KEY);
+      } catch {
+        localStorage.removeItem(LOCAL_KEY);
+      }
+    }
+
     try {
       const res = await fetch(
         "https://gist.githubusercontent.com/sevindi/8bcbde9f02c1d4abe112809c974e1f49/raw/9bf93b58df623a9b16f1db721cd0a7a539296cf0/products.json"
@@ -24,6 +33,7 @@
         console.error("Fetched data is not an array", data);
         return [];
       }
+      localStorage.setItem(LOCAL_KEY, JSON.stringify(data));
       return data;
     } catch (err) {
       console.error("Failed to fetch product data", err);
